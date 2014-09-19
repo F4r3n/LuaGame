2dce2cb38c22d06cfc59f1ae468b24adac494d11
F4r3n <guillaume_k@hotmail.fr>
2014-09-19 23:54:40 +0200

    correction bug

--- main.lua
+++ main.lua
@@ -1,10 +1,11 @@
 white= {255,255,255,255}
 black={0,0,0,255}
 blue={51,51,255,255}
+dblue={70,70,255,255}
 green={51,102,0,255}
 red = {255,0,0,255}
 orange = {255,128,0,255}
-yellow = {255,255,0,100}
+yellow = {255,255,0,255}
 grey = {96,96,96,255}
 gravity=600
 top = false
@@ -43,8 +44,11 @@ function love.load()
 	love.window.setMode(WIDTH,HEIGHT)
 	love.window.setTitle("Bounce the ball")
 
+	--Menu principal
 	menu:createButton(WIDTH/2-50,HEIGHT/10,150,50,"START",black,red,1)
 	menu:createButton(WIDTH/2-50,HEIGHT/10+200,150,50,"OPTIONS",black,red,1)
+
+	--Dans les options
 	menu:createButton(WIDTH/6,HEIGHT/2,150,50,"FULL SCREEN",black,red,2)
 	menu:createButton(WIDTH/6+200,HEIGHT/2,150,50,"OPTION2",black,red,2)
 	menu:createButton(WIDTH-200,HEIGHT-200,150,50,"RETOUR",black,yellow,2)
@@ -71,7 +75,6 @@ function love.update(dt)
 end
 
 function love.draw()
-
 	if not imenu and istart then
 		level:draw(player.x,player.y,player.size)
 		player:draw()
@@ -100,13 +103,16 @@ end
 
 function love.mousepressed(x,y,button)
 	if button == "l" then
-		menu:click(x,y)
+		if imenu or ioption then
+			menu:click(x,y)
+		end
 	end 
 end
 
 function love.keypressed(key)
 	if key=="escape" then
-		love.event.quit()
+		imenu = true
+		istart = false
 
 	elseif key=="r" then
 		love.load()
