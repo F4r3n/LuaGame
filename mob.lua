6631b9906b1131ff9d07122efbee7d5cb3f9169d
F4r3n <guillaume_k@hotmail.fr>
2014-09-20 22:01:12 +0200

    correction

--- mob.lua
+++ mob.lua
@@ -6,7 +6,8 @@ Mob = {
 		life = 5,
 		width = 30,
 		height = 30,
-		attack = 0
+		attack = 0,
+		alive = true
 	},
 
 
@@ -38,10 +39,12 @@ function Mob:create()
 	nb = 1
 	for i=1,#self.matrix-1 do
 		for j=1,#self.matrix[i] do
-			if self.matrix[i][j] == 0  and self.matrix[i+1][j] == 1 then
+			if self.matrix[i][j] == 0  and self.matrix[i+1][j] == 1 and math.random(0,1)==1 then
 				self.a[nb]={}
 				self.a[nb].x = 100*(j-1)+100-self.a.height
 				self.a[nb].y = 100*(i-1)+100-self.a.width
+				self.a[nb].alive = true
+				self.a[nb].life = 5
 
 				nb = nb+1
 			end
@@ -52,13 +55,41 @@ function Mob:create()
 end
 
 function Mob:draw(x,y,size)
-	
+
 	love.graphics.setColor(white)
 	for i=1,nb-1 do
-		love.graphics.rectangle("fill",self.a[i].x+WIDTH/2-x,self.a[i].y+HEIGHT/2-y,self.a.width,self.a.height)
+		if self.a[i] ~=nil then
+			if self.a[i].alive then
+				love.graphics.rectangle("fill",self.a[i].x+WIDTH/2-x,self.a[i].y+HEIGHT/2-y,self.a.width,self.a.height)
+			end
+		end
 	end
 
 end
+
+
+function Mob:hit(laser,x,y,level)
+	for j=1, nb-1 do
+		if laser.n > 1 then
+			for i=1,laser.n-1 do
+				if self.a[j] ~= nil then
+					if	level.collide(laser.pos[i].x,laser.pos[i].y,laser.size,laser.size,
+						self.a[j].x-x+WIDTH/2,self.a[j].y-y+HEIGHT/2,self.a.height,self.a.width) and laser.pos[i].ok and self.a[j].alive then
+						laser.pos[i].ok = false
+						self.a[j].life = self.a[j].life - 1
+						if self.a[j].life <=0 then
+							self.a[j].alive = false
+							table.remove(self.a,j)
+							nb = nb -1
+						end
+					end
+				end
+			end
+		end
+	end
+end
+
+
 
 
 return Mob
